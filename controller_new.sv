8dede2a1923e14c5582b2de6ee014dfc96f44abb
Jooa Lee <62162468+jooa32@users.noreply.github.com>
2023-02-23 14:46:47 -0800

    Update controller_new.sv

--- controller_new.sv
+++ controller_new.sv
@@ -19,9 +19,9 @@ module controller(clk, reset, pc_write, addrSrc, MemWrite, IRWrite,
         parameter FETCH = 4'b0000; //state0 Instruction Fetch
         parameter DECODE = 4'b0001; //state1  Instruction Decode / Register Fetch
         parameter MEMORY_ADDRESS = 4'b0010; //state2 Memeory address computation
-        parameter MEMORY_READ = 4'b0011; //state3 Memory access - read
+        parameter MEMORY_ACCESS_READ = 4'b0011; //state3 Memory access - read
         parameter WRITEBACK = 4'b0100; //state4 Writeback
-        parameter MEMORY_WRITE = 4'b101; //state5 Memory access - write
+        parameter MEMORY_ACCESS_WRITE = 4'b101; //state5 Memory access - write
         parameter EXECUTE = 4'b0110; //state6 Execute
         parameter Rtype = 4'b0111; //state7 R type
         parameter BRANCH = 4'b1000; //state8 Branch completion
@@ -36,17 +36,35 @@ module controller(clk, reset, pc_write, addrSrc, MemWrite, IRWrite,
                 end else begin
                     case (state)
                         FETCH: begin
-                            // pc_write <= 1'b1;
-                            // addrSrc <= 1'b0;
-                            // RegWrite <= 1'b0;
-                            // MemWrite <= 1'b0;
-                            // IRWrite <= 1'b1;
-                            // ResultSrc <= 2'b00;
-                            // if opcode = 'LW' or 'SW'
-                            if () begin
+                            // memread
+                            ALUSrca <= 00;
+                            ALUScrb <= 01;
+                            ALUControl <= 0000;
+                            pc_write;                            
+                            addrSrc;
+                            ns <= DECODE;
+                        end
+                        DECODE: begin
+                            // if lw/sw
+                            if (opcode == 0000011 || opcode == 0100011) begin
                                 ns <= MEMORY_ADDRESS;
                             end
-                        end 
+                            // if r-type
+                            else if (opcode == 0110011) begin
+                                ns <= Rtype;
+                            end
+                            // if branch
+                            else if (opcode == 1100011) begin
+                                ns <= BRANCH;
+                            end
+                            // if jal
+                            else if (opcode == 1101111) begin
+                                ns <= JUMP;
+                            end
+                        end
+                        Memory_ADDRESS: begin
+                            
+                        end
 
 
                     endcase
