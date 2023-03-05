82cea7c22fda9793be14ae8fb9a5d08557bfc414
Booker Hu <bukehu@uw.edu>
2023-03-05 15:03:18 -0800

    HW test

--- top.sv
+++ top.sv
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps
 
-module top(CLK100MHZ, BTNC, SW, LED);
+module top(CLK100MHZ, BTNC, BTNU, BTNR, SW, LED);
     input   logic               CLK100MHZ;
-    input   logic               BTNC;
+    input   logic               BTNC, BTNU, BTNR;
     input   logic [15:0]        SW;
     output  logic [15:0]        LED;
 
@@ -12,15 +12,19 @@ module top(CLK100MHZ, BTNC, SW, LED);
 
     assign reset = BTNC;
 
-    clock_divider clk_divider (.clock(CLK100MHZ), .reset(SW[15]), .divided_clocks(divided_clocks));
+    clock_divider clk_divider (.clock(CLK100MHZ), .reset(BTNU), .divided_clocks(divided_clocks));
 
-    riscv32 cpu (.clk(divided_clocks[3]), .reset(BTNC), .result_out(result_out));
+    riscv32 cpu (.clk(divided_clocks[2]), .reset(BTNC), .result_out(result_out));
 
 //    riscv32 cpu (.clk(CLK100MHZ), .reset(BTNC), .result_out(result_out));
     assign LED[0] = result_out;
     assign LED[1] = ~result_out;
     assign LED[2] = 1'b1;
     assign LED[3] = 1'b0;
+    
+    assign LED[12] = BTNR;
+    
+    assign LED[7] = SW[7];
 
 endmodule
 
@@ -28,7 +32,7 @@ endmodule
 
 module top_testbench();
     logic               CLK100MHZ;
-    logic               BTNC;
+    logic               BTNC, BTNU, BTNR;
     logic [15:0]        LED;
     logic [15:0]        SW;
     
@@ -38,7 +42,7 @@ module top_testbench();
         forever #(CLOCKPERIOD/2) CLK100MHZ <= ~CLK100MHZ;
     end
     
-    top dut (.CLK100MHZ, .SW, .BTNC, .LED);
+    top dut (.CLK100MHZ, .SW, .BTNC, .BTNU, .BTNR, .LED);
     
     initial begin
         SW[15] <= 1; @(posedge CLK100MHZ);
